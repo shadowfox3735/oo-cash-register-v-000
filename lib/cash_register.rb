cab6eae07774964080911cfab25223850ebb5310
shadowfox3735 <normanpan180@gmail.com>
2018-11-26 17:48:59 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
   
   def apply_discount
-elf.total == 0 ? "There is no discount to apply." : "After the discount, the total comes to $#{self.total -= self.total*discount/100 }."
+    self.total == 0 ? "There is no discount to apply." : "After the discount, the total comes to $#{self.total -= self.total*discount/100 }."
   end
   
 end
