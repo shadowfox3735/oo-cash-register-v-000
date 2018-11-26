2bf63e20d75c2ab9f2d5c4f864581113d9adecee
shadowfox3735 <normanpan180@gmail.com>
2018-11-26 17:33:26 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,14 @@
 class CashRegister
-  def initialize()
-    
+  attr_accessor :total
+  
+  def initialize
+    @total = 0
   end
+  
+  def add_item(title, price, quantity = 1)
+    self.total += price*quantity
+  end
+  
+  
   
 end
